9f7e129a56544157ac3b788838abafedc2ca3f5a
Robbe Derks <robbe.derks@gmail.com>
2021-03-14 22:22:32 +0100

    janky user blending

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -1,4 +1,3 @@
-from common.realtime import DT_CTRL
 from common.numpy_fast import clip, interp
 from selfdrive.car.tesla.teslacan import TeslaCAN
 from opendbc.can.packer import CANPacker
@@ -11,31 +10,16 @@ class CarController():
     self.packer = CANPacker(dbc_name)
     self.tesla_can = TeslaCAN(dbc_name, self.packer)
 
-    self.torque_blending_integral = 0
-    self.user_torque_moving_average = 0
-
-  def compute_torque_blending(self, driver_torque):
-    self.user_torque_moving_average = ((self.user_torque_moving_average * 9) + driver_torque) / 10
-    if abs(self.user_torque_moving_average) > 0.1:
-      self.torque_blending_integral += driver_torque * DT_CTRL
-    else:
-      self.torque_blending_integral *= 0.98
-
-    angle_offset = self.torque_blending_integral * 2 # TODO: tune this value
-    return angle_offset
-
   def update(self, enabled, CS, frame, actuators, cruise_cancel):
     can_sends = []
 
-    # Temp disable steering on a hands_on_fault
+    # Temp disable steering on a hands_on_fault, and allow for user override
+    # TODO: better user blending
     hands_on_fault = (CS.steer_warning == "EAC_ERROR_HANDS_ON" and CS.hands_on_level >= 3)
-    lkas_enabled = enabled and not hands_on_fault
+    lkas_enabled = enabled and (not hands_on_fault) and (CS.hands_on_level == 0)
 
     if lkas_enabled:
       apply_angle = actuators.steeringAngleDeg
-
-      # Driver torque blending
-      #apply_angle += self.compute_torque_blending(CS.out.steeringTorque)
 
       # Angular rate limit based on speed
       steer_up = (self.last_angle * apply_angle > 0. and abs(apply_angle) > abs(self.last_angle))
